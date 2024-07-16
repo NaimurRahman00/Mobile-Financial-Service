15b8cf44004caa89cab055a89dad5daee97b4834
Naimur00 <aminaim4u@gmail.com>
2024-07-16 13:35:06 +0600

    add: App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,40 @@
-import { Outlet } from "react-router-dom"
+import * as React from 'react';
+import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Dashboard from './components/Dashboard';
+import UserList from './components/User/UserList';
+import RegisterForm from './components/Auth/RegisterForm';
+import SendMoneyForm from './components/Transaction/SendMoneyForm';
+import { getUsers } from './services/api';
 
-function App() {
+const App = () => {
+  const fetchUsers = async () => {
+    try {
+      const response = await getUsers();
+      console.log(response.data);
+    } catch (error) {
+      console.error('Fetch users error:', error);
+    }
+  };
+
+  React.useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const routes = [
+    { path: '/', element: <Dashboard /> },
+    { path: '/register', element: <RegisterForm /> },
+    { path: '/users', element: <UserList /> },
+    { path: '/send-money', element: <SendMoneyForm /> },
+  ];
+
+  const router = createBrowserRouter({ routes });
+
   return (
-    <>
-      Hi ami naimur
-    </>
-  )
-}
+    <RouterProvider router={router}>
+      <Layout />
+    </RouterProvider>
+  );
+};
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,31 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import Layout from "./layout/Layout.jsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App.jsx";
+// import Layout from "./layout/Layout.jsx";
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import "./index.css";
-import Login from "./login/Login.jsx";
+// import "./index.css";
+// import Login from "./login/Login.jsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path: "/",
-        element: <App />,
-      },
-      {
-        path: "/login",
-        element: <Login />,
-      },
-    ],
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Layout />,
+//     children: [
+//       {
+//         path: "/",
+//         element: <App />,
+//       },
+//       {
+//         path: "/login",
+//         element: <Login />,
+//       },
+//     ],
+//   },
+// ]);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-);
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <RouterProvider router={router} />
+//   </React.StrictMode>
+// );
