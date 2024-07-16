14c9e2bad7dbd11aa27f0f88aeb1bc6ef9549cb6
Naimur00 <aminaim4u@gmail.com>
2024-07-16 11:38:48 +0600

    Create: MFS project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl bg-black">Hi, I'm Naimur</h1>
+      <button className="btn">Button</button>
+    </>
+  )
+}
+
+export default App
